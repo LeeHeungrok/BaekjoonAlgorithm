cf3cfad25e2d35250a12cedb57a42588565ba417
LeeHeungRok <rhh0208@naver.com>
2018-03-27 14:47:55 +0900

    Sum C++ 추가

---
+++ Level_1/2_A+B/Sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int left, right, result;
+    std::cin>>left;
+    std::cin>>right;
+
+    result = left + right;
+    
+    std::cout<<result<<std::endl;
+}
