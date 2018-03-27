e5fc3e80e1cb72587826eb0073ca39e3e0a74491
LeeHeungRok <rhh0208@naver.com>
2018-03-27 14:44:31 +0900

    Sum Java 추가

---
+++ Level_1/2_A+B/Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Sum{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int left, right, result;
+
+        left = scan.nextInt();
+        right = scan.nextInt();
+
+        result = left + right;
+        System.out.println(result);
+    }
+}
