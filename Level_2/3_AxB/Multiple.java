ebe4d9b6c0a219bf62e0fe9d33474c550e7295e3
LeeHeungRok <rhh0208@naver.com>
2018-03-27 21:33:57 +0900

    2단계 시작 / Multiple Java 작성

---
+++ Level_2/3_AxB/Multiple.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Multiple{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int left, right, result;
+
+        left = scan.nextInt();
+        right = scan.nextInt();
+
+        result = left * right;
+
+        System.out.println(result);
+        scan.close();
+    }
+}
