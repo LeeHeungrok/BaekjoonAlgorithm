dfd6e46f3b2984fa374c58398d4362c2b8b3123a
LeeHeungRok <rhh0208@naver.com>
2018-03-27 20:34:09 +0900

    Same Output C++ 작성

---
+++ Level_1/7_SameOutput/SameOutput.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+    string input;
+    int count = 0;
+
+    while(count < 100){
+        cin>>input;
+        if(input.length() > 100){
+            input.erase(100, input.length() - 100);
+        }
+        cout<<input<<endl;
+    }
+    return 0;
+}
