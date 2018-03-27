19454b5133c08a9cf983c3c9ee853a86744f81a2
LeeHeungRok <rhh0208@naver.com>
2018-03-27 18:46:06 +0900

    SameOutput Java 작성중 이상.. 흑..

--- Level_1/7_SameOutput/SameOutput.java
+++ Level_1/7_SameOutput/SameOutput.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 class SameOutput{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        String input;
+        // int count = 0;
 
+        input = scan.next();
+        // if(scan.hasNextLine()){
+        //     if(input.length() > 100){
+        //         input.substring(100, input.length());
+        //     }
+        // }
+        
+        System.out.println(input);
+        
         scan.close();
     }
 }
