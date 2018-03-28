bf09cce03fdc7a933cc1ced2be7308d93c596e1e
LeeHeungRok <rhh0208@naver.com>
2018-03-28 12:28:45 +0900

    Divide C++ 작성

---
+++ Level_2/4_AdivideB/Divide.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double left, right, result;
+
+    cin>>left;
+    cin>>right;
+    
+    result = left / right;
+
+    cout<<result<<endl;
+    
+    return 0;
+}
