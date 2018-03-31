28acd34e4dc1f8b271b0a67aa31d2ed25ae0ddc6
이흥록 <leeheungrok@iheunglog-ui-MacBook-Pro.local>
2018-03-31 23:16:56 +0900

    Operation C++ 작성

---
+++ Level_2/5_Operation/Operation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int left, right;
+
+    cin>>left>>right;
+    
+    cout<<left + right<<endl;
+    cout<<left - right<<endl;
+    cout<<left * right<<endl;
+    cout<<left / right<<endl;
+    cout<<left % right<<endl;
+    
+    return 0;
+}
