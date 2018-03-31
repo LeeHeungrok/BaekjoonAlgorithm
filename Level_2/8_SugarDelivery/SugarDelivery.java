e64660f38939d0ac1819eeaa6eec7f11b643acb9
이흥록 <leeheungrok@iheunglog-ui-MacBook-Pro.local>
2018-04-01 00:03:21 +0900

    Suger Delivery Java 작성

--- Level_2/8_SugarDelivery/SugarDelivery.java
+++ Level_2/8_SugarDelivery/SugarDelivery.java
@@ -3,9 +3,28 @@ import java.util.Scanner;
 class SugarDelivery{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        
+
         final int THREE_KG = 3;
         final int FIVE_KG = 5;
+        int suger, count = 0;
 
+        suger = scan.nextInt();
+
+        count = suger / FIVE_KG;
+
+        suger = suger % FIVE_KG;
+
+        count = suger / THREE_KG;
+
+        suger = suger % THREE_KG;
+
+        if(suger != 0){
+            System.out.println(-1);
+        }
+        else {
+            System.out.println(count);
+        }
+        
+        scan.close();
     }
 }
