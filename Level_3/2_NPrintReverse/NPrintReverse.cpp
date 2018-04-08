9eac3d9eac85eb94a485d94e0bc02b56054a7d40
LeeHeungrok <rhh0208@naver.com>
2018-04-08 22:42:44 +0900

    N Print Reverse C++ 작성

---
+++ Level_3/2_NPrintReverse/NPrintReverse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input;
+
+    cin>>input;
+
+    for(int index = input; index > 0; index --){
+        cout<<index<<"\n";
+    }
+    return 0;
+}
