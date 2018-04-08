237a57391afeccb2d048ad166e546ff2dacfa8a9
LeeHeungrok <rhh0208@naver.com>
2018-04-08 22:30:23 +0900

    NPrint C++ 작성

---
+++ Level_3/1_NPrint/NPrint.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int index, input;
+
+    cin>>input;
+
+    for(index = 1; index <= input; index ++){
+        cout<<index<<endl;
+    }
+    return 0;
+}
