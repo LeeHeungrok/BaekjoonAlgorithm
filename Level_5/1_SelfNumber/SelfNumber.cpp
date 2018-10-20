7ca38b9927104ab915c643e7bce34f59810ac42e
이흥록 <leeheungrok@LeeHeungRokui-MacBook-Pro.local>
2018-10-20 17:42:19 +0900

    셀프 넘버 품

---
+++ Level_5/1_SelfNumber/SelfNumber.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int selfNumber(int digit);
+
+int main(void){
+    bool checkSelfNumber[10001] = {false,};
+
+    for(int index = 1; index < 10000; index ++){
+        int number = selfNumber(index);
+        checkSelfNumber[number] = true;
+    } 
+    
+    for(int index = 1; index < 10000; index ++){
+        if(!checkSelfNumber[index]){
+            cout<<index<<endl;
+        }
+    }
+}
+
+int selfNumber(int digit){
+    int result = digit;
+    for(int index = 10000; index != 1; index /= 10){
+        if(digit / index > 0){
+            result += digit / index;
+            digit %= index;
+        }
+    }
+    result += digit;
+
+    return result;
+}
