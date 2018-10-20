d51c0fb71a4691a004fa49c071058462cc9fa7a7
이흥록 <leeheungrok@LeeHeungRokui-MacBook-Pro.local>
2018-10-21 01:20:21 +0900

    한수 완성

--- Level_5/2_OneNumber/OneNumber.cpp
+++ Level_5/2_OneNumber/OneNumber.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
+int findNumber(int number);
 
 int main(void){
+    int inputNumber;
+
+    cin>>inputNumber;
+
+    cout<<findNumber(inputNumber)<<endl;
     
+    return 0;
+}
+
+int findNumber(int number){
+    if(number < 100){
+        return number;
+    }
+
+    int first;
+    int second;
+    int third;
+    int result = 99;
+
+    for(int index = 100; index <= number; index ++){
+        first = index / 100 % 10;
+        second = index / 10 % 10;
+        third = index % 10;
+
+        if(first + third == second * 2){
+            result ++;
+        }
+    }
+
+    return result;
 }
