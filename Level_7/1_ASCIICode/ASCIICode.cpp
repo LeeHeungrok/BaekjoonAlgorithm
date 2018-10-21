a7fe339d33c77ef19c77e6c2ad6a8aa57d87d7d0
이흥록 <leeheungrok@LeeHeungRokui-MacBook-Pro.local>
2018-10-21 19:48:25 +0900

    아스키코드 작성

---
+++ Level_7/1_ASCIICode/ASCIICode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    char input;
+    int change;
+    cin>>input;
+
+    change = input;
+
+    cout<<change<<endl;
+    return 0;
+}
