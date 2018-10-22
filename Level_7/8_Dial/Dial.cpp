102cc2f64b444034cd991d793bdc16daba97cb9e
이흥록 <leeheungrok@LeeHeungRokui-MacBook-Pro.local>
2018-10-22 12:47:18 +0900

    switch 문으로 변경, 하나의 숫자에 알파벳이 4개일 수도 있다는걸 파악하지 못함

--- Level_7/8_Dial/Dial.cpp
+++ Level_7/8_Dial/Dial.cpp
@@ -12,15 +12,15 @@ int main(void){
     cin >> inputAlphabet;
     
     for(int index = 0; index < inputAlphabet.length(); index ++){
-        int temp = inputAlphabet[index] - '0';
-        if(temp == 0){
-            result += 3;
-        }
-        else if(temp == 1){
-            result += 2;
-        }
-        else {
-            result += ((inputAlphabet[index] - 65) / 3) + 3;
+        switch(inputAlphabet[index]){
+            case 'A': case 'B': case 'C': result += 3; break;
+            case 'D': case 'E': case 'F': result += 4; break;
+            case 'G': case 'H': case 'I': result += 5; break;
+            case 'J': case 'K': case 'L': result += 6; break;
+            case 'M': case 'N': case 'O': result += 7; break;
+            case 'P': case 'Q': case 'R': case 'S': result += 8; break;
+            case 'T': case 'U': case 'V': result += 9; break;
+            case 'W': case 'X': case 'Y': case 'Z': result += 10; break;
         }
     }
     
