89a239b0a798b75b9574e7eb9b281cee2de0ad7d
이흥록 <leeheungrok@LeeHeungRokui-MacBook-Pro.local>
2018-10-26 01:54:55 +0900

    크로아티아 알파벳 완료

---
+++ Level_7/9_CroatiaAlphabet/CroatiaAlphabel.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    string input;
+    string croatia[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+    int count = 0;
+
+    cin >> input;
+    
+    for(int index = 0; index < 8; index ++){
+        size_t pos = 0;
+        while(pos < input.length()){
+            if((pos = input.find(croatia[index], pos)) != string::npos){
+                cout<<input[pos]<<endl;
+                input.replace(pos, croatia[index].length(), "*");
+            }
+        }
+    }
+
+    cout<<input.length()<<endl;
+
+    return 0;
+}
