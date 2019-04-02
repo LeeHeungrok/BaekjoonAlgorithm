fca6ce5729938df64099074e9276325ca5e11c8a
LeeHeungrok <rhh0208@naver.com>
2019-04-02 19:40:37 +0900

    계수정렬 메모리초과와 시간초과 오류 해결

--- Level_9/3_CountingSort.cpp
+++ Level_9/3_CountingSort.cpp
@@ -2,67 +2,35 @@
 
 using namespace std;
 
-// int InputInt()
-// {
-//     int iInput;
-
-//     while (true)
-//     {
-//         cin >> iInput;
-
-//         if (cin.fail())
-//         {
-//             cin.clear();
-//             cin.ignore(1024, '\n');
-//             continue;
-//         }
-//         break;
-//     }
-
-//     return iInput;
-// }
-
-void CountingSort(int* arr, int* count, int size)
-{
-    int* result = new int[size + 1];
-
-    // 카운트 배열에 카운팅을 앞에서부터 더해가져 저장
-    for (int i = 1; i <= size; ++i)
-    {
-        count[i] += count[i - 1];
-    }
-
-    // arr 배열의 값의 카운트 배열 인덱스로 접근하고 카운트 배열의 값을 result에 저장
-    for (int i = size - 1; i >= 0; --i)
-    {
-        result[count[arr[i]]] = arr[i];
-        count[arr[i]] --;
-    }
-
-    // 정렬된 값 출력
-    for (int i = 1; i <= size; ++i)
-    {
-        cout << result[i] << endl;
-    }
-}
-
 int main()
 {
     int iLength;
 
+    // 시간초과 해결
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
     cin >> iLength;
     
-    int iArr[iLength];
-    int* count = new int[iLength + 1];
+    int count[10001] = {};
 
+    int iInput;
     for (int i = 0; i < iLength; ++i)
     {
-        scanf("%d", &iArr[i]);
-        // 각 나온 Value를 카운트 배열에 카운팅
-        count[iArr[i]] ++;
+        cin >> iInput;
+        count[iInput] ++;
     }
 
-    CountingSort(iArr, count, iLength);
-    
+    for (int i = 1; i < 10001; ++i)
+    {
+        if (count[i] > 0)
+        {
+            for (int j = 0; j < count[i]; ++j)
+            {
+                cout << i << '\n';
+            }
+        }
+    }
+
     return 0;
 }
